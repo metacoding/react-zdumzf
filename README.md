98a2d24d790d77f52c39351a880c8f1ee2490e62
metacoding <mehran.taherimoud@gmail.com>
2019-04-19 19:00:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-zdumzf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-zdumzf)
---
+++ index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+import FileViewer from 'react-file-viewer';
+
+const pngFile = 'https://upload.wikimedia.org/wikipedia/commons/7/70/Example.png';
+const docxFile = 'https://www2.le.ac.uk/Members/davidwickins/old/test.docx/at_download/file';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.onError = this.onError.bind(this);
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Doc Viewer</h1>
+        <p>
+          FileViewer for PNG files
+          <FileViewer
+            fileType="png"
+            filePath={pngFile}
+            errorComponent={this.CustomErrorComponent}
+            onError={this.onError}/>
+        </p>
+        <p>
+          FileViewer for PNG files
+          <FileViewer
+            fileType="docx"
+            filePath={docxFile}
+            errorComponent={this.CustomErrorComponent}
+            onError={this.onError}/>
+        </p>
+      </div>
+    );
+  }
+
+  onError(e) {
+    console.log('error in file-viewer', e);
+  }
+}
+
+render(<App />, document.getElementById('root'));
